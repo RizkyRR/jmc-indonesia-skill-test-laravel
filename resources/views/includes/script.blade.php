16c8c99e5f18803d38f51815c56cfecf25e6c513
Rizky Rahmadianto <rahmadianto018@gmail.com>
2023-07-24 11:09:11 +0700

    second innit

---
+++ resources/views/includes/script.blade.php
@@ -0,0 +1,46 @@
+<!-- Core JS -->
+<!-- build:js assets/vendor/js/core.js -->
+<script src="{{ asset('admin-assets/assets/vendor/libs/popper/popper.js') }}"></script>
+<script src="{{ asset('admin-assets/assets/vendor/js/bootstrap.js') }}"></script>
+<script src="{{ asset('admin-assets/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js') }}"></script>
+
+<script src="{{ asset('admin-assets/assets/vendor/js/menu.js') }}"></script>
+<!-- endbuild -->
+
+<!-- Vendors JS -->
+<script src="{{ asset('admin-assets/assets/vendor/libs/apex-charts/apexcharts.js') }}"></script>
+
+<!-- Main JS -->
+<script src="{{ asset('admin-assets/assets/js/main.js') }}"></script>
+
+<!-- Page JS -->
+<script src="{{ asset('admin-assets/assets/js/dashboards-analytics.js') }}"></script>
+
+{{-- select 2 --}}
+<script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+
+{{-- DATATABLES --}}
+<script src="{{ asset('admin-assets/libs/datatables/datatables.min.js') }}"></script>
+<script src="{{ asset('admin-assets/libs/datatables/DataTables-1.12.1/js/dataTables.bootstrap4.min.js') }}"></script>
+<script src="{{ asset('admin-assets/libs/datatables/FixedHeader-3.2.3/js/fixedHeader.bootstrap4.min.js') }}"></script>
+<script src="{{ asset('admin-assets/libs/datatables/Responsive-2.3.0/js/responsive.bootstrap4.min.js') }}"></script>
+<script src="{{ asset('admin-assets/libs/datatables/Responsive-2.3.0/js/responsive.dataTables.min.js') }}"></script>
+<script
+  src="{{ asset('admin-assets/libs/datatables/jquery-datatables-checkboxes-1.2.12/js/dataTables.checkboxes.min.js') }}">
+</script>
+<script src="https://cdn.datatables.net/buttons/2.4.1/js/dataTables.buttons.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.10.1/jszip.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
+<script src="https://cdn.datatables.net/buttons/2.4.1/js/buttons.html5.min.js"></script>
+
+{{-- jquery tambahan --}}
+<script src="{{ asset('admin-assets/js/jquery.form.min.js') }}"></script>
+<script src="{{ asset('admin-assets/js/jquery.mask.min.js') }}"></script>
+<script src="{{ asset('admin-assets/js/jquery-file-upload.js') }}"></script>
+
+{{-- sweetalert --}}
+<script src="{{ asset('admin-assets/vendor/sweetalert/sweetalert.min.js') }}"></script>
+
+<!-- Place this tag in your head or just before your close body tag. -->
+<script async defer src="https://buttons.github.io/buttons.js"></script>
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+
+<html lang="en" class="light-style layout-menu-fixed" dir="ltr" data-theme="theme-default"
+  data-assets-path="{{ asset('admin-assets') }}" data-template="vertical-menu-template-free">
+
+<head>
+  <title>@yield('title') | Nama Situs</title>
+
+  @include('includes.meta')
+
+  @stack('before-style')
+  @include('includes.style')
+  @stack('after-style')
+</head>
+
+<body>
+  <!-- Layout wrapper -->
+  <div class="layout-wrapper layout-content-navbar">
+    <div class="layout-container">
+      <!-- Menu -->
+      @include('includes.navbar')
+      <!-- / Menu -->
+
+      <!-- Layout container -->
+      <div class="layout-page">
+        <!-- Navbar -->
+        @include('includes.header')
+        <!-- / Navbar -->
+
+        <!-- Content wrapper -->
+        <div class="content-wrapper">
+          <!-- Content -->
+
+          <div class="container-xxl flex-grow-1 container-p-y">
+            @yield('content')
+          </div>
+          <!-- / Content -->
+
+          <!-- Footer -->
+          @include('includes.footer')
+          <!-- / Footer -->
+
+          <div class="content-backdrop fade"></div>
+        </div>
+        <!-- Content wrapper -->
+      </div>
+      <!-- / Layout page -->
+    </div>
+
+    <!-- Overlay -->
+    <div class="layout-overlay layout-menu-toggle"></div>
+  </div>
+  <!-- / Layout wrapper -->
+
+  <div class="buy-now">
+    <a href="https://themeselection.com/products/sneat-bootstrap-html-admin-template/" target="_blank"
+      class="btn btn-danger btn-buy-now">Upgrade to Pro</a>
+  </div>
+
+  @stack('before-script')
+  @include('includes.script')
+  @stack('after-script')
+</body>
+
+</html>
